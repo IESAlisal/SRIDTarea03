271f9072891d2752b2a322b00de973568ba95c0e
Javier Terán González <javiteran@hotmail.com>
2023-11-04 22:04:20 +0100

    Update menu.php

--- menu.php
+++ menu.php
@@ -10,9 +10,8 @@
     echo "<p>ID de instancia: " . @file_get_contents("http://instance-data/latest/meta-data/instance-id");
     echo "<p>Hostname: "        . @file_get_contents("http://instance-data/latest/meta-data/public-hostname");
 ?>
-
-<hr class="my-4">
 <p>Utilizar Apache/PHP. La BBDD es mysql y será de sólo lectura</p>
+<hr class="my-4">
 <ul class="nav nav-tabs">
     <li class="nav-item"><a class="nav-link<?php if($pagina=="/index.php"){  echo ' active'; }?>" href="index.php">Personas</a></li>
     <li class="nav-item"><a class="nav-link<?php if($pagina=="/alumnos.php"){  echo ' active'; }?>" href="alumnos.php">Alumnos</a></li>
