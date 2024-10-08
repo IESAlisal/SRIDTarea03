fc0122e53c84cb7d85a52d6973e373c9d93d63dc
Javier Terán González <javier.teran@educantabria.es>
2024-10-08 09:57:38 +0200

    Delete vars.php

--- vars.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $BBDDServidor='ec2-18-207-59-96.compute-1.amazonaws.com';
-    $BBDDUsuario='sritarea03';
-    $BBDDPassword='sritarea03pass';
-    $BBDD='sritarea03';
-    $pagina=$_SERVER['SCRIPT_NAME']; 
-?>
